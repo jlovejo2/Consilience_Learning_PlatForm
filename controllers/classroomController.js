f7d392687f341dfde9a0b8fc4b19fe978aec6a0d
James Lovejoy <james.lovejoy2@gmail.com>
2020-03-29 10:31:02 -0500

    added a find by Id, and create method

--- controllers/classroomController.js
+++ controllers/classroomController.js
@@ -7,4 +7,20 @@ module.exports = {
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
-   
+    findById: function(req, res) {
+        db.ClassroomModel
+          .findById(req.params.id)
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err));
+      },
+      create: function(req, res) {
+          console.log(req.body);
+        db.ClassroomModel
+          .create(req.body)
+          .then(dbModel => {
+              res.json(dbModel)
+                console.log('book saved');
+            })
+          .catch(err => res.status(422).json(err));
+      },
+     
