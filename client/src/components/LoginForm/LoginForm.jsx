b39353edc487232f5d1e9ddda09a929d41a7aa27
James Lovejoy <james.lovejoy2@gmail.com>
2020-03-30 20:17:10 -0500

    fixed css classes on login.jsx for materialize and react

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Form(props) {
 
     return (
-        <form>
+        <form className={props.size}>
             {props.children}
         </form>
     )
@@ -11,13 +11,17 @@ export function Form(props) {
 
 export function Input(props) {
 
+    function Capitalize(str){
+        return str.charAt(0).toUpperCase() + str.slice(1);
+        }
+
     return (
-    <div className="row">
-        <div className="input-field hoverable col">
+        <div className={`hoverable col ${props.size}`}>
+            <label>
+                {Capitalize(props.name)}:
             <input placeholder={props.placeholder} name={props.name} required type={props.requiredType} className={`${props.customClass}`} />
-            <label htmlFor={props.name}><p>{props.label}</p></label>
+            </label>
         </div>
-    </div>
     );
 };
 
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -8,32 +8,40 @@ const Login = () => {
         <Container fluid>
             {/* <div className="text-white">Login</div> */}
         <div className='row valign-wrapper'>
-            <div className='col s2'></div>
-            <div className='col s8 center-align m6'>
+            <div className='col s4'></div>
+            <div className='col s6 center-align'>
             <Card 
             title={<i>Login Here</i>} 
+            size={'medium'}
             color={'blue-grey'} 
             customClass={'darken-1 center'}
             action={<button type="submit" className="waves-effect waves-light btn-large">LOGIN</button>}
             bottomLink={<p>Don't have an account? <a href="/users/register">Register Here!</a></p>}
             >
-                <Form>
+            <div class='row'>
+                <Form size={'col s12'}>
+                    <div class='row'>
                     <Input
+                    size='s12'
                     name='email'
                     label='Email :'
                     placeholder='Please enter your email address'
                     requiredType='email'
                     customClass='validate center' />
+                                        
                     <Input
+                    size='s12'
                     name='password'
                     label='Password :'
                     placeholder='Please enter your password'
                     requiredType='password'
                     customClass='validate center' />
+                    </div>
                 </Form>
+                </div>
             </Card>
             </div>
-            <div className='col s2'></div>
+            <div className='col s4'></div>
         </div>   
         </Container>
 
