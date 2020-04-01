4978ade18b995e0e3f8ba879a5e56b045d8a0690
Joe Shields <shields.john.joseph@gmail.com>
2020-04-01 00:22:06 -0500

    jumbotron styling change

--- client/src/components/Jumbotron/Jumbotron.jsx
+++ client/src/components/Jumbotron/Jumbotron.jsx
@@ -8,7 +8,7 @@ const Jumbotron = () => {
     <MDBContainer className='jumbocontain mx-auto col-md-8 py-5'>
           <MDBJumbotron className="jumboimg" style={{ backgroundImage: `url(https://drive.google.com/uc?id=1ApvW1VY_lYNnefiSIPMhPBCraHtkUs2d)` }}>
                 <MDBCardTitle className="jumbotitle col-md-8 mx-auto">Consilience</MDBCardTitle>
-                <p className="jumbotext">We are consilience a hub to keep you connected with all of your classroom needs.</p>
+                <p className="jumbotext">We are Consilience, a hub to keep you connected with all of your classroom needs.</p>
                 <MDBBtn className="btn-center"> Login/Signup</MDBBtn>
           </MDBJumbotron>
     </MDBContainer>
