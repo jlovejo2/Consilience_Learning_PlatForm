4e389b2b14be1457881fbbea4a9828f9cca9fdef
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-31 19:52:55 -0500

    added unique parameter to email

--- models/register.js
+++ models/register.js
@@ -44,7 +44,8 @@ const RegisterSchema = new Schema(
             email: {
                 type: String,
                 validate: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                required: true
+                required: true,
+                unique: true
             },
             password: {
                 type: String,
