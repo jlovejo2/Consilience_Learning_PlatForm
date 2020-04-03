c87159003da5a377f05c1d559baa3e6eca75e423
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-04-03 04:22:01 -0500

    passport-jwt

--- config/passport.js
+++ config/passport.js
@@ -1,23 +1,23 @@
-const passport = require('passport');
-const BearerStrategy = require('passport-http-bearer')
-const RegisterModel = require('../models/register.js')
-// https://www.npmjs.com/package/passport-http-bearer
+// https://www.npmjs.com/package/passport-jwt
+const db = require('../models');
 
-//passport for authentication and authorization
-passport.use(new BearerStrategy(
-    function(accessToken, done) {
-        RegisterModel.findOne({ accessToken })
-            .then((userFound) => {
-                if (userFound) {
-                    return done(null, userFound)
-                } else {
-                    return done(null, false)
-                }
-            })
-            .catch((error) => {
-                done(error)
-            })
+const JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+let opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'secret';
+opts.issuer = 'accounts.examplesoft.com';
+opts.audience = 'http://localhost:3003';
+passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    db.RegisterModel.findOne({id: jwt_payload.sub}, function(err, user) {
+        if (err) {
+            return done(err, false);
         }
-  ));
-
-  module.exports = passport
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));
