c1d116cfa72f9c4cf377fb62f4475843d50cdaa5
unknown <angeyuangeyu@gmail.com>
2020-04-06 00:25:44 -0500

    new layout bc i forgot what i was doing

--- client/src/components/Assignments/Selection.jsx
+++ client/src/components/Assignments/Selection.jsx
@@ -1,93 +1,40 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import Grow from '@material-ui/core/Grow';
-import Paper from '@material-ui/core/Paper';
-import Popper from '@material-ui/core/Popper';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList';
-import './style.css';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
-
-const options = ['Class 1', 'Class 2', 'Class 3'];
-
-export default function Selection() {
-  const [open, setOpen] = React.useState(false);
-  const anchorRef = React.useRef(null);
-  const [selectedIndex, setSelectedIndex] = React.useState(1);
-
-  const handleClick = () => {
-    console.info(`You clicked ${options[selectedIndex]}`);
-  };
-
-  const handleMenuItemClick = (event, index) => {
-    setSelectedIndex(index);
-    setOpen(false);
-  };
-
-  const handleToggle = () => {
-    setOpen((prevOpen) => !prevOpen);
-  };
-
-  const handleClose = (event) => {
-    if (anchorRef.current && anchorRef.current.contains(event.target)) {
-      return;
-    }
-
-    setOpen(false);
-  };
-
+export default function FormControlLabelPosition() {
   return (
-    <React.Fragment>
-    <Grid container direction="column" alignItems="center">
-      <Grid item xs={12}>
-        <ButtonGroup variant="contained" color="primary" ref={anchorRef} aria-label="split button">
-          <Button onClick={handleClick}>{options[selectedIndex]}</Button>
-          <Button
-            color="primary"
-            size="small"
-            aria-controls={open ? 'split-button-menu' : undefined}
-            aria-expanded={open ? 'true' : undefined}
-            aria-label="select merge strategy"
-            aria-haspopup="menu"
-            onClick={handleToggle}
-          >
-            <ArrowDropDownIcon />
-          </Button>
-        </ButtonGroup>
-        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
-          {({ TransitionProps, placement }) => (
-            <Grow
-              {...TransitionProps}
-              style={{
-                transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom',
-              }}
-            >
-              <Paper>
-                <ClickAwayListener onClickAway={handleClose}>
-                  <MenuList id="split-button-menu">
-                    {options.map((option, index) => (
-                      <MenuItem
-                        key={option}
-                        disabled={index === 2}
-                        selected={index === selectedIndex}
-                        onClick={(event) => handleMenuItemClick(event, index)}
-                      >
-                        {option}
-                      </MenuItem>
-                    ))}
-                  </MenuList>
-                </ClickAwayListener>
-              </Paper>
-            </Grow>
-          )}
-        </Popper>
-      </Grid>
-    </Grid>
-    </React.Fragment>
+    <FormControl component="fieldset">
+      <FormLabel component="legend">Select classes from which to view assignments:</FormLabel>
+      <FormGroup aria-label="position" row>
+        <FormControlLabel
+          value="{CLASS1}"
+          control={<Checkbox color="primary" />}
+          label="{Class 1}"
+          labelPlacement="end"
+        />
+        <FormControlLabel
+          value="{CLASS2}"
+          control={<Checkbox color="primary" />}
+          label="{Class 2}"
+          labelPlacement="end"
+        />
+        <FormControlLabel
+          value="{CLASS3}"
+          control={<Checkbox color="primary" />}
+          label="{Class 3}"
+          labelPlacement="end"
+        />
+        <FormControlLabel
+          value="{CLASS4}"
+          control={<Checkbox color="primary" />}
+          label="{Class 4}"
+          labelPlacement="end"
+        />
+      </FormGroup>
+    </FormControl>
   );
 }
-
