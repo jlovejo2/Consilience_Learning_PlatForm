691a204565d452039fbc1d24dd9554ffbc604673
Joe Shields <shields.john.joseph@gmail.com>
2020-04-08 02:37:38 -0500

    style update

--- client/src/components/ClassCard/ClassCard.jsx
+++ client/src/components/ClassCard/ClassCard.jsx
@@ -1,47 +1,52 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import clsx from 'clsx';
-import './style.css';
-import { Card, CardHeader, CardMedia, CardContent, CardActions } from '@material-ui/core';
-import Collapse from '@material-ui/core/Collapse';
-import { makeStyles } from '@material-ui/core/styles';
-import Avatar from '@material-ui/core/Avatar';
-import Badge from '@material-ui/core/Badge';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
-import MailIcon from '@material-ui/icons/Mail';
-import CreateIcon from '@material-ui/icons/Create';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import clsx from "clsx";
+import "./style.css";
+import {
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+} from "@material-ui/core";
+import Collapse from "@material-ui/core/Collapse";
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import Badge from "@material-ui/core/Badge";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import MeetingRoomIcon from "@material-ui/icons/MeetingRoom";
+import MailIcon from "@material-ui/icons/Mail";
+import CreateIcon from "@material-ui/icons/Create";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     // backgroundColor: '#61dbfb',
     // background: 'repeating-radial-gradient(circle farthest-side at bottom left, rgb(255, 255, 255) 58%, rgb(97, 219, 251) 89%)',
-    borderRadius: 35,
+    borderRadius: "35px",
     // boxShadow: '0 3px 5px 2px rgba(191, 191, 191, .3)',
-    width: '90%',
-    height: '100%',
-    display: 'inline-grid',
+    width: "90%",
+    height: "100%",
+    display: "inline-grid",
     // background: 'rgb(66,66,66)',
     // background: 'linear-gradient(254deg, rgba(66,66,66,1) 0%, rgba(97,219,251,1) 100%)',
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%", // 16:9
   },
   expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
       duration: theme.transitions.duration.shortest,
     }),
   },
   expandOpen: {
-    transform: 'rotate(180deg)',
+    transform: "rotate(180deg)",
   },
   avatar: {
     backgroundColor: red[500],
@@ -52,20 +57,22 @@ export default function ClassCard(props) {
   const classes = useStyles();
   const [expanded, setExpanded] = useState(false);
 
-
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
 
   return (
-    <Card className={classes.root} value={props.classID} >
-
-    <CardHeader data-classid={props.classID}
-        avatar={
-          <Avatar alt="Teacher Image" src={props.teacherAvatar} />
-        }
+    <Card className={classes.root} value={props.classID}>
+      <CardHeader
+        data-classid={props.classID}
+        avatar={<Avatar alt="Teacher Image" src={props.teacherAvatar} />}
         action={
-          <IconButton aria-label="settings" aria-controls='simple-menu' onClick={props.settingsButton} data-classid={props.classID} >
+          <IconButton
+            aria-label="settings"
+            aria-controls="simple-menu"
+            onClick={props.settingsButton}
+            data-classid={props.classID}
+          >
             <MoreVertIcon />
           </IconButton>
         }
@@ -74,12 +81,16 @@ export default function ClassCard(props) {
       />
       {/*------------------------ This is the end of CardHeader ---------------------------*/}
       {/*----------------------- This is the beginning of Card Image and Caption Location------------- */}
-      { props.image ? <CardMedia
-        className={classes.media}
-        image={props.image}
-        title={props.imageTitle}
-        src=''
-      /> : '' }
+      {props.image ? (
+        <CardMedia
+          className={classes.media}
+          image={props.image}
+          title={props.imageTitle}
+          src=""
+        />
+      ) : (
+        ""
+      )}
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
           {props.imageCaption}
@@ -92,14 +103,16 @@ export default function ClassCard(props) {
           <CreateIcon />
         </IconButton>
         <IconButton>
-        <Link to={{
-          pathname: '/classrooms',
-          state: {
-            classroomID: props.classID
-          }
-        }} >
-          <MeetingRoomIcon />
-        </Link>
+          <Link
+            to={{
+              pathname: "/classrooms",
+              state: {
+                classroomID: props.classID,
+              },
+            }}
+          >
+            <MeetingRoomIcon />
+          </Link>
         </IconButton>
         <Badge badgeContent={4} color="secondary">
           <MailIcon />
@@ -120,12 +133,10 @@ export default function ClassCard(props) {
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
           <Typography paragraph>Description: </Typography>
-          <Typography paragraph>
-            {props.paragraph1}
-          </Typography>
+          <Typography paragraph>{props.paragraph1}</Typography>
         </CardContent>
       </Collapse>
       {/*----------------------- This is the End of Card Expanded menu, essentially description of Class------------- */}
-    </Card >
+    </Card>
   );
 }
