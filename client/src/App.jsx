1de29749bf97e8f9afbc33d3cf8ac62c9e55eef2
unknown <angeyuangeyu@gmail.com>
2020-04-08 13:22:23 -0500

    deleting activities

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Home from './pages/home.jsx';
-import Activities from './pages/activitiesTeacher.jsx';
 import Assignments from './pages/assignmentsTeacher.jsx';
 import dashboardTeacher from './pages/dashboardTeacher.jsx';
 import dashboardStudent from './pages/dashboardStudent'
@@ -33,7 +32,6 @@ const App = () => {
                 <Navbar />
                 <Wrapper>
                     <React.Fragment>
-                        <Route exact path="/activities" component={Activities} />
                         {/* <Route exact path="/search" component={Search} /> */}
                         <Route exact path="/assignments" component={Assignments} />
                         <Route exact path="/dashboardTeacher" component={dashboardTeacher} />
