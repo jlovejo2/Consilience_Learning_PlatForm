b98c6399bf04b9f376ef93e0c552977cef8492ee
James Lovejoy <james.lovejoy2@gmail.com>
2020-04-08 14:45:20 -0500

    added conditional rendering for user to classroom and classCard, removed all unnecessary code from dashboardStudent

--- client/src/pages/dashboardStudent.jsx
+++ client/src/pages/dashboardStudent.jsx
@@ -1,26 +1,17 @@
-import React, { useState, useEffect } from "react";
-// import ClassroomContext from "../utils/classroomContext";
+import React, { useState, useEffect, useContext } from "react";
+import RootContext from "../utils/RootContext";
 import API from "../utils/API";
 import custFunc from "../utils/customFunctions";
 //Importing components from component folder
 import Container from "../components/Container/Container.jsx";
 import ClassCard from "../components/ClassCard/ClassCard";
 //Importing components and icons from material-ui
-// import Paper from '@material-ui/core/Paper';
+
 import Card from "@material-ui/core/Card";
 import { styled } from "@material-ui/core/styles";
-// import Fab from '@material-ui/core/Fab';
-import {
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-} from "@material-ui/core";
-import { Button, Input, TextField } from "@material-ui/core";
-import { Menu, MenuItem } from "@material-ui/core";
+
 import Grid from "@material-ui/core/Grid";
-// import AddIcon from '@material-ui/icons/Add';
+
 // import NavigationIcon from '@material-ui/icons/Navigation';
 // import Paper from '@material-ui/core/Paper';
 // import { makeStyles } from '@material-ui/core/styles';
@@ -29,18 +20,16 @@ const MyCard = styled(Card)({
   background: "transparent",
 });
 const DashboardStudent = (props) => {
-  const [openDialog, setOpenDialog] = useState(false);
-  const [newClassFormObj, setNewClassFormObj] = useState({});
+
+  const { userType, userID } = useContext(RootContext)
+
   const [classesArr, setClassesArr] = useState([]);
-  const [menuAnchor, setMenuAnchor] = useState(null);
-  const [selectedFile, setSelectedFile] = useState({});
-  const [currentClass, setCurrentClass] = useState("");
 
   useEffect(() => {
-    // eslint-disable-next-line
+
     loadClasses();
-    // eslint-disable-next-line
-  }, [newClassFormObj]);
+  
+  }, []);
 
   //This function calls the backend and loads all the classes in the database onto the dashboard page
   //Eventually this function will only load the classes that the user has access too
@@ -72,71 +61,7 @@ const DashboardStudent = (props) => {
       .catch((err) => console.log(err));
   }
 
-  function handleDialogClose() {
-    setOpenDialog(false);
-  }
-  // eslint-disable-next-line
-  function handleCreateClass() {
-    setOpenDialog(true);
-  }
 
-  function handleMenuClick(event) {
-    setMenuAnchor(event.currentTarget);
-    setCurrentClass(event.currentTarget.dataset.classid);
-  }
-
-  function handleMenuClose() {
-    setMenuAnchor(null);
-  }
-
-  function fileSelectHandler(event) {
-    setSelectedFile(event.target.files[0]);
-  }
-
-  function updateClassImage() {
-    console.log(selectedFile);
-    console.log(currentClass);
-    const fd = new FormData();
-
-    fd.set("image", selectedFile, selectedFile.name);
-
-    API.updateClassImage(currentClass, fd)
-      .then((resp) => {
-        console.log("image saved");
-        console.log(resp);
-      })
-      .catch((err) => console.log(err));
-  }
-
-  function handleChangeTitle() {
-    return <input type="email" placeholder="enter title info"></input>;
-  }
-
-  //This function is called by the input tags and textarea tags on the dailog form for the add a class button
-  //It places the content the user is typing into those tags into the newClassFormObj so that it can be submitted upon button click
-  function handleInputChange(event) {
-    console.log(event.target.name);
-    const { name, value } = event.target;
-    setNewClassFormObj({ ...newClassFormObj, [name]: value });
-  }
-
-  //This function is called by the submit button on the create class dialog form.  It takes all the information from the class dialog form that
-  //has been updated and placed into newClassFormObj by onChange and submits it to the backend through the API.addclass() function
-  function handleDailogSubmit() {
-    //This is being done manually because user model and authentication is still being worked on
-    const currentUserID = "077432";
-
-    newClassFormObj.userID = currentUserID;
-
-    console.log(newClassFormObj);
-    API.addClass(newClassFormObj)
-      .then((resp) => {
-        console.log("Class added successfully");
-        loadClasses();
-        handleDialogClose();
-      })
-      .catch((err) => console.log(err));
-  }
   // const classes = useStyles();
 
   return (
@@ -171,8 +96,9 @@ const DashboardStudent = (props) => {
                       image={item.imageBase64Str}
                       imageTitle=""
                       imageCaption=""
-                      settingsButton={handleMenuClick}
+                      // settingsButton={handleMenuClick}
                       classID={item._id}
+                      usertype={userType}
                     ></ClassCard>
                   </Grid>
                 );
@@ -181,89 +107,7 @@ const DashboardStudent = (props) => {
               <p>No classes Found</p>
             )}
         </Grid>
-        {/* --------------------------------------------------------------------- */}
-        {/*______________ Below this line is menu for class cards________________ */}
-        {/* ----------------------------------------------------------------------*/}
-        <Menu
-          id="simple-menu"
-          anchorEl={menuAnchor}
-          keepMounted
-          open={Boolean(menuAnchor)}
-          onClose={handleMenuClose}
-        >
-          <MenuItem>
-            <label>
-              Add Image to Class: &nbsp;
-              <input type="file" onChange={fileSelectHandler} />
-            </label>
-            <button onClick={updateClassImage}>UPLOAD</button>
-          </MenuItem>
-          <MenuItem onClick={handleChangeTitle}>Change Title</MenuItem>
-          <MenuItem onClick={handleMenuClose}>Update Description</MenuItem>
-        </Menu>
-      </MyCard>
-      {/* --------------------------------------------------------------------------------------------- */}
-      {/*<________________________ Below This Line is Dialog Form for adding class __________________>  */}
-      {/* --------------------------------------------------------------------------------------------- */}
-      <Dialog
-        open={openDialog}
-        onClose={handleDialogClose}
-        aria-labelledby="form-dialog-title"
-      >
-        <DialogTitle id="form-dialog-title">Add a Class</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Please Enter your classroom Information below ...
-          </DialogContentText>
-          <label>
-            Course Title:
-            <Input
-              autoFocus
-              disableUnderline
-              margin="dense"
-              id="title"
-              name="title"
-              type="text"
-              fullWidth
-              onChange={handleInputChange}
-            />
-          </label>
-          <label>
-            Course Discipline:
-            <Input
-              autoFocus
-              disableUnderline
-              margin="dense"
-              id="discipline"
-              name="discipline"
-              type="text"
-              fullWidth
-              onChange={handleInputChange}
-            />
-          </label>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="description"
-            name="description"
-            label="Course Description"
-            type="text"
-            variant="outlined"
-            multiline
-            rowsMax="4"
-            fullWidth
-            onChange={handleInputChange}
-          />
-          <DialogActions>
-            <Button onClick={handleDialogClose} color="primary">
-              Cancel
-            </Button>
-            <Button onClick={handleDailogSubmit} color="primary" type="submit">
-              Submit
-            </Button>
-          </DialogActions>
-        </DialogContent>
-      </Dialog>
+        </MyCard>
     </Container>
   );
 };
