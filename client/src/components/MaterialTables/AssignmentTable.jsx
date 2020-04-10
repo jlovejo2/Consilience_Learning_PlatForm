bccb69d8e97f9bf7c35d94b460f96b67a32ffd98
James Lovejoy <james.lovejoy2@gmail.com>
2020-04-10 13:20:46 -0500

    made material tables into separate components for grades and assignments

---
+++ client/src/components/MaterialTables/AssignmentTable.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react';
+import MaterialTable from 'material-table';
+import API from '../../utils/API';
+
+
+function AssignmentTable (props) {
+
+    const [classID, setClassID] = useState()
+    const [assignments, setAssignments] = useState({
+        columnsAssignment: [
+            { title: "Title", field: "title" },
+            { title: "Description", field: "description" },
+            { title: "Attachments", field: "attachments" },
+        ],
+        data: props.assignments.map(assignment => {
+            return assignment
+        })
+
+    })
+
+    useEffect(() => {
+        console.log(assignments)
+
+        setClassID(localStorage.getItem('classId'))
+
+        
+    }, [assignments, classID])
+
+return (
+    <MaterialTable
+    title="Assignments"
+    columns={assignments.columnsAssignment}
+    data={assignments.data}
+    actions={[
+        {
+          icon: 'save',
+          tooltip: 'Save User',
+          onClick: (event, rowData) => {
+              API.createAssignment(classID, rowData)
+                .then(resp => {
+                    console.log(resp)
+                })
+                .catch(err => console.log(err))
+          }
+        },
+    ]}
+    editable={{
+        onRowAdd: newData =>
+            new Promise(resolve => {
+                setTimeout(() => {
+                    resolve();
+                    setAssignments(prevState => {
+                        console.log(prevState)
+                        const data = [...prevState.data];
+                        console.log(newData)
+                        data.push(newData);
+                        console.log(data);
+                        return { ...prevState, data }
+                    });
+                    console.log(assignments)
+                }, 1000);
+            }),
+        onRowUpdate: (newData, oldData) =>
+            new Promise(resolve => {
+                setTimeout(() => {
+                    resolve();
+                    if (oldData) {
+                        setAssignments(prevState => {
+                            const data = [...prevState.data];
+                            data[data.indexOf(oldData)] = newData;
+                            return { ...prevState, data };
+                        });
+                    }
+                }, 600);
+            }),
+        onRowDelete: oldData =>
+            new Promise(resolve => {
+                setTimeout(() => {
+                    resolve();
+                    setAssignments(prevState => {
+                        const data = [...prevState.data];
+                        data.splice(data.indexOf(oldData), 1);
+                        return { ...prevState, data };
+                    });
+                }, 600);
+            })
+    }}
+/>
+
+)
+
+}
+
+
+export default AssignmentTable;
