331910bc2fbc3b6095821f074f9204ed78841a34
James Lovejoy <james.lovejoy2@gmail.com>
2020-05-17 17:42:16 -0500

    added mission statement to homepage

--- client/src/components/Jumbotron/Jumbotron.jsx
+++ client/src/components/Jumbotron/Jumbotron.jsx
@@ -1,19 +1,25 @@
 import React from "react";
-import { MDBJumbotron, MDBBtn, MDBContainer, MDBCardTitle} from "mdbreact";
-import './Jumbotron.css'
-
+import { MDBJumbotron, MDBBtn, MDBContainer, MDBCardTitle } from "mdbreact";
+import "./Jumbotron.css";
 
 const Jumbotron = (props) => {
   return (
-    <MDBContainer className='jumbocontain'>
-          <MDBJumbotron className="jumboimg animated bounceInDown" style={{ backgroundImage: `url(https://i.ytimg.com/vi/Gg_9Cc3NKK8/maxresdefault.jpg)` }}>
-                <MDBCardTitle className="jumbotitle">Consilience</MDBCardTitle>
-{/* Andrew Write your statement in the P tag below*/}
-                <p className="jumbotext"></p>
-                <MDBBtn className="btn-grad center" href={props.href}> Login/Register</MDBBtn>
-          </MDBJumbotron>
+    <MDBContainer className="jumbocontain">
+      <MDBJumbotron
+        className="jumboimg animated bounceInDown"
+        style={{
+          backgroundImage: `url(https://i.ytimg.com/vi/Gg_9Cc3NKK8/maxresdefault.jpg)`,
+        }}
+      >
+        <MDBCardTitle className="jumbotitle">Consilience</MDBCardTitle>
+        <p className="jumbotext">{props.missionStatement}</p>
+        <MDBBtn className="btn-grad center" href={props.href}>
+          {" "}
+          Login/Register
+        </MDBBtn>
+      </MDBJumbotron>
     </MDBContainer>
-  )
-}
+  );
+};
 
 export default Jumbotron;
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,16 +1,18 @@
-import React /*, { useState, useEffect }*/ from 'react';
-import Container from '../components/Container/Container.jsx'
-import Jumbotron from '../components/Jumbotron/Jumbotron.jsx'
-
-
+import React /*, { useState, useEffect }*/ from "react";
+import Container from "../components/Container/Container.jsx";
+import Jumbotron from "../components/Jumbotron/Jumbotron.jsx";
 
 const Home = () => {
-    
-    return (
-            <Container fluid>
-                <Jumbotron href='/login' />
-            </Container>
-    )
-}
+  return (
+    <Container fluid>
+      <Jumbotron
+        missionStatement={
+          'Welcome to Consilience. Consilience is the principle that evidence from independent, unrelated sources can "converge" on strong conclusions.  This platform exists for a user to either teach, be taught, or both.  Here a user can register as a "Teacher" or "Student."  If you do not want to register just yet click on the search icon in navbar and browse our classes.  One can never learn to much!'
+        }
+        href="/login"
+      />
+    </Container>
+  );
+};
 
 export default Home;
