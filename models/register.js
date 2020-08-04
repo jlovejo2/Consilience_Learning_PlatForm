c659eae21a2eada238bf2015c9c0ff1d1e63e4b1
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-04 10:25:17 -0500

    changed eslint format in register.js

--- models/register.js
+++ models/register.js
@@ -1,95 +1,95 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 // const validate = require('validator');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 // const moment = require('moment');
 //const dateChicago = moment.parseZone()
 //const dateChicago = moment().tz("America/Chicago").format();
 
 const RegisterSchema = new Schema(
   {
-            type: {
-                type: String,
-                trim: true,
-                // enum: ["student", "teacer"],
-                required: "select role",
-                // validate: `student` || `teacher` 
-            },
-            firstName: {
-                type: String,
-                trim: true,
-                required: 'enter first name'
-            },
-            lastName: {
-                type: String,
-                trim: true,
-                required: 'enter last name'
-            },
-            ID: {
-                // type: Schema.Types.ObjectId,
-                type: String,
-                ref: 'User',
-            },
-            discipline: {
-                type: String,
-                trim: true
-                },
-            // TOUCH ON THIS LATER
-            // activeCourses: {
-            //     type: String,
-            //     trim: true,
-            // },
-            // completedCourses: {
-            //     type: Array,
-            //     trim: true
-            // },
-            email: {
-                type: String,
-                validate: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                // validate: value => {
-                //     if (!validator.isEmail(value)) {
-                //         throw new Error({ error: 'Invalid Email address' })
-                //     }
-                // },
-                required: true,
-                unique: true
-            },
-            password: {
-                type: String,
-                // validate: /^[0-9A-Za-z!@.,;:'"?-]{8,50}\z/,
-                required: true,
-                minlength: 8,
-                maxlength: 64
-            },
-            grades: [
-                {
-                  classId: {
-                    type: Schema.Types.ObjectId,
-                    ref: 'Classroom',
-                  },
-                  assignment: {
-                    type: String,
-                    trim: true,
-                  },
-                  grade: {
-                    type: String,
-                    trim: true,
-                  }
-                }
-              ],
-            // // tokens: [{
-            //     token: {
-            //         type: Array,
-            //         // required: true
-            //     },
-            // }],
-            createDate: {
-                type: Date,
-                required: true,
-                default: Date.now
-            }
+    type: {
+      type: String,
+      trim: true,
+      // enum: ["student", "teacer"],
+      required: "select role",
+      // validate: `student` || `teacher`
+    },
+    firstName: {
+      type: String,
+      trim: true,
+      required: "enter first name",
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      required: "enter last name",
+    },
+    ID: {
+      // type: Schema.Types.ObjectId,
+      type: String,
+      ref: "User",
+    },
+    discipline: {
+      type: String,
+      trim: true,
+    },
+    // TOUCH ON THIS LATER
+    // activeCourses: {
+    //     type: String,
+    //     trim: true,
+    // },
+    // completedCourses: {
+    //     type: Array,
+    //     trim: true
+    // },
+    email: {
+      type: String,
+      validate: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      // validate: value => {
+      //     if (!validator.isEmail(value)) {
+      //         throw new Error({ error: 'Invalid Email address' })
+      //     }
+      // },
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      // validate: /^[0-9A-Za-z!@.,;:'"?-]{8,50}\z/,
+      required: true,
+      minlength: 8,
+      maxlength: 64,
+    },
+    grades: [
+      {
+        classId: {
+          type: Schema.Types.ObjectId,
+          ref: "Classroom",
         },
+        assignment: {
+          type: String,
+          trim: true,
+        },
+        grade: {
+          type: String,
+          trim: true,
+        },
+      },
+    ],
+    // // tokens: [{
+    //     token: {
+    //         type: Array,
+    //         // required: true
+    //     },
+    // }],
+    createDate: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+  },
 
   // Mongoose Virtuals https://mongoosejs.com/docs/tutorials/virtuals.html
   // a property not stored in MongoDB
@@ -97,8 +97,8 @@ const RegisterSchema = new Schema(
   // setting virtuals to true to pass properties to response.json()
   {
     toJSON: {
-      virtuals: true
-    }
+      virtuals: true,
+    },
   }
 );
 
@@ -110,7 +110,6 @@ const RegisterSchema = new Schema(
 //     await user.save()
 //     return token
 // }
-
 
 // Mongoose Methods Github https://github.com/Automattic/mongoose
 // possible methods PRE HOOK
