815783e5d40c450ab7540e33ccec416b31ed252f
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-04 10:26:00 -0500

    added 5 student and 2 teacher users to be seeded into database

--- scripts/seedAtlas.js
+++ scripts/seedAtlas.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const mongo = require("mongodb");
 const mongoose = require("mongoose");
 const db = require("../models");
+const userFuncs = require("../controllers/functions");
 
 const users = [
   {
@@ -11,9 +12,89 @@ const users = [
     lastName: "Tribiani",
     email: "jtrib@gmail.com",
     password: "password_**********",
-    ID: "jtribian0",
+    ID: "",
     grades: [],
     creatDate: new Date(),
+  },
+  {
+    _id: new mongo.ObjectId("5f29698dda48af2f3c8b8171"),
+    type: "Student",
+    firstName: "Mary",
+    lastName: "Life",
+    email: "mlife@gmail.com",
+    password: "password_**********",
+    ID: "",
+    grades: [],
+    creatDate: new Date(),
+  },
+  {
+    _id: new mongo.ObjectId("5f29698dda48af2f3c8b8172"),
+    type: "Student",
+    firstName: "Fred",
+    lastName: "George",
+    email: "fGeorge@gmail.com",
+    password: "password_**********",
+    ID: "",
+    grades: [],
+    creatDate: new Date(),
+  },
+  {
+    _id: new mongo.ObjectId("5f29698dda48af2f3c8b8173"),
+    type: "Student",
+    firstName: "Rachel",
+    lastName: "Green",
+    email: "rGreen@gmail.com",
+    password: "password_**********",
+    ID: "",
+    grades: [],
+    creatDate: new Date(),
+  },
+  {
+    _id: new mongo.ObjectId("5f29698dda48af2f3c8b8174"),
+    type: "Student",
+    firstName: "Chandler",
+    lastName: "Bing",
+    email: "cBing@gmail.com",
+    password: "password_**********",
+    ID: "",
+    grades: [],
+    creatDate: new Date(),
+  },
+  {
+    _id: new mongo.ObjectId("5f29698dda48af2f3c8b8175"),
+    type: "Teacher",
+    firstName: "Ross",
+    lastName: "Gellar",
+    discipline: "Paleantology",
+    email: "rGellar@gmail.com",
+    password: "password_**********",
+    ID: "",
+    grades: [],
+    creatDate: new Date(),
+  },
+  {
+    _id: new mongo.ObjectId("5f29698dda48af2f3c8b8176"),
+    type: "Teacher",
+    firstName: "Phoebe",
+    lastName: "Buffet",
+    discipline: "Music",
+    email: "pBuffet@gmail.com",
+    password: "password_**********",
+    ID: "",
+    grades: [],
+    creatDate: new Date(),
+  },
+];
+
+const classes = [
+  {
+    courseDiscipline: "",
+    courseTitle: "",
+    courseDescription: "",
+    students: [{}, {}],
+    teacherID: "",
+    assignments: [{}, {}],
+    createDate: new Date(),
   },
 ];
 
@@ -34,6 +115,21 @@ const seed = async () => {
     );
 
     for (const user of users) {
+      let userID = "";
+
+      if (user.type === "Teacher") {
+        userID = await userFuncs.staffIDGenerator(
+          user.firstName,
+          user.lastName,
+          user.Discipline
+        );
+      } else {
+        userID = await userFuncs.studentIdGenerator(
+          user.firstName,
+          user.lastName
+        );
+      }
+      user.ID = userID;
       console.log(user);
       console.log(db.registers);
       await db.RegisterModel.collection.insertOne(user);
