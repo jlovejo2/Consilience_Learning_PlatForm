a7e1361c0239d6c6079298f7135ca633056a7862
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-04 18:13:53 -0500

    placed logout in useCallback function to solve useEffect issues

--- client/src/pages/logout.jsx
+++ client/src/pages/logout.jsx
@@ -1,48 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import API from '../utils/API.js';
-import JumbotronLogout from '../components/Jumbotron/JumbotronLogout.jsx';
-import Container from '../components/Container/Container.jsx';
-import history from '../history/history.jsx';
+import React, { useState, useEffect, useCallback } from "react";
+import API from "../utils/API.js";
+import JumbotronLogout from "../components/Jumbotron/JumbotronLogout.jsx";
+import Container from "../components/Container/Container.jsx";
+import history from "../history/history.jsx";
 
 const Logout = () => {
+  const [userType, setUserType] = useState("");
+  const [userID, setUserID] = useState("");
 
-    const [userType, setUserType] = useState("");
-    const [userID, setUserID] = useState("")
+  // boilerplate
+  const getAndVerifyUserInfo = useCallback(() => {
+    API.readAndVerifyCookie()
+      .then((resp) => {
+        console.log("cookie call resp: ", resp);
+        console.table("dropping the load: ", resp.data.payload);
+        setUserType(resp.data.payload.type);
+        setUserID(resp.data.payload._id);
+        console.log(userType);
+        console.log(userID);
+      })
+      .catch((error) => {
+        console.log(error);
+        history.replace("/");
+      });
+  }, [userID, userType]);
+  // lifecycle hook
+  useEffect(() => {
+    getAndVerifyUserInfo();
+  }, [getAndVerifyUserInfo]);
 
-    // boilerplate
-    function getAndVerifyUserInfo() {
-        API.readAndVerifyCookie()
-            .then((resp) => {
-                console.log("cookie call resp: ", resp)
-                console.table("dropping the load: ", resp.data.payload)
-                setUserType(resp.data.payload.type)
-                setUserID(resp.data.payload._id)
-                console.log(userType)
-                console.log(userID)
-            })
-            .catch(error => {
-                console.log(error)
-                history.replace('/')
-            })
-    }
-    // lifecycle hook
-    useEffect(() => {
-        getAndVerifyUserInfo()
-    }, [userType, userID])
-
-    function logoutUser () {
-        API.userAuthLogout(userID)
-        console.log("logging out")
-        history.replace('/')
-    }
-    // api to use ====> 
-    // API.userLogout (by id, userID) ==> replaces cookie containing JWT with
-    // new cookie (same name) containing ephemeral JWT (lifespan of 1 millisecond)
-    return (<Container fluid>
-                <JumbotronLogout logout={logoutUser}/>
-            </Container>
-    )
- 
-}
+  function logoutUser() {
+    API.userAuthLogout(userID);
+    console.log("logging out");
+    history.replace("/");
+  }
+  // api to use ====>
+  // API.userLogout (by id, userID) ==> replaces cookie containing JWT with
+  // new cookie (same name) containing ephemeral JWT (lifespan of 1 millisecond)
+  return (
+    <Container fluid>
+      <JumbotronLogout logout={logoutUser} />
+    </Container>
+  );
+};
 
 export default Logout;
